68ce4a9cb9bb6cf342d0ba8dede54aeadee36bf8
saveroo <saveroo27@gmail.com>
2020-09-26 02:00:25 +0700

    fix port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 ﻿const API = require("./api")
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4747
 
 app.get('/api', API)
 app.get('/', express.static("./"))
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`Started at port: ${port}`)
 })
