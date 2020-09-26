b9601f15d621220237ef883c2b8ecb1d05196cfb
saveroo <saveroo27@gmail.com>
2020-09-26 17:13:22 +0700

    (WIP) Implement Wappalyzer

--- api/index.js
+++ api/index.js
@@ -1,16 +1,38 @@
 const hibp = require("../libbitch/crap");
+const wapp = require("../libbitch/wapp");
 
 const API = async (req, res) => {
-    console.log(req, 'body');
+
     const email = req.body.email;
-    let results
-    try{
-        results = await hibp(email);
-    } catch (err) {
-        console.log("Err", err)
-    }
+    const domain = req.body.domain
+    const cmd = req.body.cmd;
+
     res.setHeader('Cache-Control', 's-maxage=5400, stale-while-revalidate')
-    results ? res.json(results) : res.json({});
+
+    if(cmd == null)
+        return;
+
+    let results
+    switch (cmd) {
+        case "email":
+                try{
+                    results = await hibp(email);
+                } catch (err) {
+                    console.error("Err", err)
+                }
+            break;
+        case "domain":
+                try {
+                    results = await wapp(domain);
+                } catch (err) {
+                    console.error("Err", err)
+                }
+            break;
+        default:
+            break;
+    }
+
+    res.json(results)
 } 
 
 module.exports = API
---
+++ libbitch/wapp.js
@@ -0,0 +1,29 @@
+﻿const wappalyzer = require("wappalyzer");
+
+async function checkDomain(url) {
+    const options = {
+        debug: false,
+        delay: 500,
+        headers: {},
+        maxDepth: 3,
+        maxUrls: 10,
+        maxWait: 5000,
+        recursive: true,
+        probe: true,
+        userAgent: 'Wappalyzer',
+        htmlMaxCols: 2000,
+        htmlMaxRows: 2000,
+    };
+    const wapp = await new wappalyzer(options)
+    try {
+        await wapp.init()
+        const site = await wapp.open("https://" + url)
+        const result = await site.analyze()
+        return result;
+    } catch (err) {
+        console.error(err)
+    }
+    await wapp.destroy()
+}
+
+module.exports = checkDomain
