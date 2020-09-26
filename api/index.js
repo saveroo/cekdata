eac14008904a88f047fd11b4a8f7412aea22190d
saveroo <saveroo27@gmail.com>
2020-09-26 13:35:48 +0700

    API cache control

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const hibp = require("../libbitch/crap");
 const API = async (req, res) => {
     const email = req.query["email"];
     const results = await hibp(email);
+    res.setHeader('Cache-Control', 's-maxage=5400, stale-while-revalidate')
     res.json(results);
 } 
 
