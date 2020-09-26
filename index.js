8dfe8663edb1ef7060fff64f9878d84e687b116a
saveroo <saveroo27@gmail.com>
2020-09-26 08:50:34 +0700

    Utilize Setup/Compose API

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4747
+const path = require("path")
 
+app.use('/assets', express.static(path.join(__dirname, './assets')))
 app.get('/api', API)
-app.get('/', express.static("./"))
+app.get('/', express.static(path.join(__dirname, '.')))
 
 app.listen(port, () => {
   console.log(`Started at port: ${port}.`)
